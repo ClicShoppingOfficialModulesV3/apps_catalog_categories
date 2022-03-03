30de88acb516c7651a29312d44b88e9ae658afaf
ClicShopping <admin@clicshopping.org>
2022-03-03 22:32:32 +0100

    update

--- includes/ClicShopping/Apps/Catalog/Categories/Module/ClicShoppingAdmin/Config/ConfigAbstract.php
+++ includes/ClicShopping/Apps/Catalog/Categories/Module/ClicShoppingAdmin/Config/ConfigAbstract.php
@@ -43,7 +43,7 @@
 
     public function install()
     {
-      $cut_length = strlen('CLICSHOPPING_APP_CATEGORIES_' . $this->code . '_');
+      $cut_length = \strlen('CLICSHOPPING_APP_CATEGORIES_' . $this->code . '_');
 
       foreach ($this->getParameters() as $key) {
         $p = strtolower(substr($key, $cut_length));
@@ -52,7 +52,7 @@
 
         $cfg = new $class($this->code);
 
-        $this->app->saveCfgParam($key, $cfg->default, isset($cfg->title) ? $cfg->title : null, isset($cfg->description) ? $cfg->description : null, isset($cfg->set_func) ? $cfg->set_func : null);
+        $this->app->saveCfgParam($key, $cfg->default, $cfg->title ?? null, $cfg->description ?? null, $cfg->set_func ?? null);
       }
     }
 
@@ -97,7 +97,7 @@
 
       $cut = 'CLICSHOPPING_APP_CATEGORIES_' . $this->code . '_';
 
-      $cut_length = strlen($cut);
+      $cut_length = \strlen($cut);
 
       foreach ($this->getParameters() as $key) {
         $p = strtolower(substr($key, $cut_length));
@@ -108,7 +108,7 @@
 
 
         if (!\defined($key)) {
-          $this->app->saveCfgParam($key, $cfg->default, isset($cfg->title) ? $cfg->title : null, isset($cfg->description) ? $cfg->description : null, isset($cfg->set_func) ? $cfg->set_func : null);
+          $this->app->saveCfgParam($key, $cfg->default, $cfg->title ?? null, $cfg->description ?? null, $cfg->set_func ?? null);
         }
 
         if ($cfg->app_configured !== false) {
